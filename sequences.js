8cf0a9881f11b1a50901ee22386706aed7827403
emmanuel.gautier <emmanuel.gre@gmail.com>
2018-03-01 18:39:36 +0100

    Super nasty integration of Stalactite. Will need more work

---
+++ sequences.js
@@ -0,0 +1,394 @@
+
+
+// var csv_string = ""
+//   // csv_string += "folder,10\n"
+//   // csv_string += "file,1\n"
+//   // csv_string += "hu.txt,6\n"
+//   // csv_string += "test-swag.com,3\n"
+//   // csv_string += "test-swag.fr,2\n"
+//   // csv_string += "test-up-t.com,3\n"
+//   // csv_string += "test-up-o.com,3\n"
+
+
+// csv_string += "Test Git-.DS_Store,6148\n"
+// csv_string += "Test Git-test_vim.txt,1\n"
+// csv_string += "Test Git-test_first_repo-dude.png,13659\n"
+// csv_string += "Test Git-test_first_repo-dummy_text_file.txt,103\n"
+// csv_string += "Test Git-test_first_repo-README.md,47\n"
+// csv_string += "Test Git-test_first_repo-.git-COMMIT_EDITMSG,20\n"
+// csv_string += "Test Git-test_first_repo-.git-config,324\n"
+// csv_string += "Test Git-test_first_repo-.git-description,73\n"
+// csv_string += "Test Git-test_first_repo-.git-HEAD,23\n"
+// csv_string += "Test Git-test_first_repo-.git-index,297\n"
+// csv_string += "Test Git-test_first_repo-.git-packed_refs,114\n"
+// csv_string += "Test Git-test_first_repo-.git-refs-remotes-origin-HEAD,32\n"
+// csv_string += "Test Git-test_first_repo-.git-refs-remotes-origin-master,41\n"
+// csv_string += "Test Git-test_first_repo-.git-refs-heads-master,41\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-f2-75f29a34efe7bfd00c9a9b684467d92315ce3f,168\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-e2-7547dfd9d8e0c02db76217987e6b7c25d46d94,168\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-e1-6e1689f24935c8a3cb1bd6a330eb4daa176ddd,127\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-b1-1f79d5a6500682c709ce350957862968a52653,13183\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-76-bbe94a33430ca9b0a74ef9d3edec411b0f9d1a,529\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-75-a5664f1b34f0dd80959d6629b17a3a3e5fa22d,73\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-6a-3b461fd19253096e01f0a59b60d795335ef248,169\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-4c-7adc903717af1db0109dde8317c4eb95c383d0,96\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-31-d10795296109cc1dcce24d4e6b6f518a55b56e,99\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-2e-2e995ca02bcba5f6df6452a07826ae32d69e5d,167\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-26-8888065e48b9d78600846d26305ef033d98ec4,58\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-13-736da1eb5f88c856c62ef92e6c945ccd30933a,127\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-09-83eb018794999079fbf62f7d9d7ce40632d4b8,93\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-08-e3a35d6388cf7a4a74b66ae4004f0b39ec4620,127\n"
+// csv_string += "Test Git-test_first_repo-.git-objects-00-9a85bfc7a65fe07c280074889bf3001756c7e9,53\n"
+// csv_string += "Test Git-test_first_repo-.git-logs-HEAD,891\n"
+// csv_string += "Test Git-test_first_repo-.git-logs-refs-remotes-origin-HEAD,208\n"
+// csv_string += "Test Git-test_first_repo-.git-logs-refs-remotes-origin-master,312\n"
+// csv_string += "Test Git-test_first_repo-.git-logs-refs-heads-master,891\n"
+// csv_string += "Test Git-test_first_repo-.git-info-exclude,250\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-applypatch_msg.sample,478\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-commit_msg.sample,896\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-post_update.sample,189\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-pre_applypatch.sample,424\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-pre_commit.sample,1642\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-pre_push.sample,1348\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-pre_rebase.sample,4898\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-pre_receive.sample,544\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-prepare_commit_msg.sample,1492\n"
+// csv_string += "Test Git-test_first_repo-.git-hooks-update.sample,3610\n"
+// csv_string += "Test Git-repo1-test.txt,26\n"
+// csv_string += "Test Git-repo1-.git-COMMIT_EDITMSG,25\n"
+// csv_string += "Test Git-repo1-.git-config,137\n"
+// csv_string += "Test Git-repo1-.git-description,73\n"
+// csv_string += "Test Git-repo1-.git-HEAD,41\n"
+// csv_string += "Test Git-repo1-.git-index,137\n"
+// csv_string += "Test Git-repo1-.git-refs-heads-master,41\n"
+// csv_string += "Test Git-repo1-.git-objects-e6-9de29bb2d1d6434b8b29ae775ad8c2e48c5391,15\n"
+// csv_string += "Test Git-repo1-.git-objects-e0-b7bf70f163cca656c571de2d4dba4889b4e1df,162\n"
+// csv_string += "Test Git-repo1-.git-objects-c8-4e687c0452510233b55a0f1fa091abae132c24,53\n"
+// csv_string += "Test Git-repo1-.git-objects-ab-2c1538600de7da499f5369fbede239c2df2c3c,176\n"
+// csv_string += "Test Git-repo1-.git-objects-8c-6f74cf1eabf2ccfc870b9ab911f6ba7ccbbe02,53\n"
+// csv_string += "Test Git-repo1-.git-objects-6a-3f5574c68fe1eabcf76380ae166f38d51b5403,53\n"
+// csv_string += "Test Git-repo1-.git-objects-5e-fb9bc29c482e023e40e0a2b3b7e49cec842034,53\n"
+// csv_string += "Test Git-repo1-.git-objects-4b-825dc642cb6eb9a060e54bf8d69288fbee4904,15\n"
+// csv_string += "Test Git-repo1-.git-objects-17-00a5af6792e715d9b1551d34656d27b9c937c7,37\n"
+// csv_string += "Test Git-repo1-.git-objects-11-a70e034f5827b019b62de36f9ae67bcfa4ba0e,140\n"
+// csv_string += "Test Git-repo1-.git-logs-HEAD,726\n"
+// csv_string += "Test Git-repo1-.git-logs-refs-heads-master,512\n"
+// csv_string += "Test Git-repo1-.git-info-exclude,250\n"
+// csv_string += "Test Git-repo1-.git-hooks-applypatch_msg.sample,478\n"
+// csv_string += "Test Git-repo1-.git-hooks-commit_msg.sample,896\n"
+// csv_string += "Test Git-repo1-.git-hooks-post_update.sample,189\n"
+// csv_string += "Test Git-repo1-.git-hooks-pre_applypatch.sample,424\n"
+// csv_string += "Test Git-repo1-.git-hooks-pre_commit.sample,1642\n"
+// csv_string += "Test Git-repo1-.git-hooks-pre_push.sample,1348\n"
+// csv_string += "Test Git-repo1-.git-hooks-pre_rebase.sample,4898\n"
+// csv_string += "Test Git-repo1-.git-hooks-pre_receive.sample,544\n"
+// csv_string += "Test Git-repo1-.git-hooks-prepare_commit_msg.sample,1492\n"
+// csv_string += "Test Git-repo1-.git-hooks-update.sample,3610\n"
+
+function plot(csv_string) {
+
+  d3.select("#chart").selectAll("svg").remove()
+  d3.select("#legend").selectAll("svg").remove()
+
+
+  // Dimensions of sunburst.
+  var width = 800;
+  var height = 300;
+
+  // Breadcrumb dimensions: width, height, spacing, width of tip/tail.
+  var b = {
+    w: 75, h: 30, s: 3, t: 10
+  };
+
+  // Mapping of step names to colors.
+  var colors = {
+    folder : "#e59310",
+    file : "#bcbcbc"
+  };
+
+
+  function colorOf(name, children) {
+    var m = name.match(/\..*$/)
+    if (children !== undefined) {
+      return colors.folder
+    } else {
+      return colors.file
+    }
+  }
+
+  // Total size of all segments; we set this later, after loading the data.
+  var totalSize = 0; 
+
+  var vis = d3.select("#chart").append("svg:svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("svg:g")
+      .attr("id", "container");
+
+  var partition = d3.layout.partition()
+      .size([width, height])
+      .value(function(d) { return d.size; });
+
+  // Use d3.text and d3.csv.parseRows so that we do not need to have a header
+  // row, and can receive the csv as an array of arrays.
+
+  var csv = d3.csv.parseRows(csv_string);
+  var json = buildHierarchy(csv);
+  createVisualization(json);
+
+
+
+  // Main function to draw and set up the visualization, once we have the data.
+  function createVisualization(json) {
+
+    // Basic setup of page elements.
+    initializeBreadcrumbTrail();
+    drawLegend();
+    d3.select("#togglelegend").on("click", toggleLegend);
+
+    // Bounding rect underneath the chart, to make it easier to detect
+    // when the mouse leaves the parent g.
+    vis.append("svg:rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("opacity", 0);
+
+    // For efficiency, filter nodes to keep only those large enough to see.
+    var nodes = partition.nodes(json)
+        .filter(function(d) {
+        return (d.dx > 0.5);
+        });
+
+    var node = vis.data([json]).selectAll(".node")
+        .data(nodes)
+        .enter().append("rect")
+        .attr("class", "node")
+        .attr("x", function(d) { return d.x; })
+        .attr("y", function(d) { return d.y; })
+        .attr("width", function(d) { return d.dx; })
+        .attr("height", function(d) { return d.dy; })
+        .attr("display", function(d) { return d.depth ? null : "none"; })
+        .style("fill", function(d) { return colorOf(d.name, d.children); })
+        .style("opacity", 1)
+        .on("mouseover", mouseover);
+
+    // Add the mouseleave handler to the bounding rect.
+    d3.select("#container").on("mouseleave", mouseleave);
+
+    // Get total size of the tree = value of root node from partition.
+    totalSize = node.node().__data__.value;
+   };
+
+  // Fade all but the current sequence, and show it in the breadcrumb trail.
+  function mouseover(d) {
+
+    var percentage = (100 * d.value / totalSize).toPrecision(3);
+    var percentageString = percentage + "%";
+    if (percentage < 0.1) {
+      percentageString = "< 0.1%";
+    }
+
+    var sequenceArray = getAncestors(d);
+    updateBreadcrumbs(sequenceArray, percentageString);
+
+    // Fade all the segments.
+    d3.selectAll(".node")
+        .style("opacity", 0.3);
+
+    // Then highlight only those that are an ancestor of the current segment.
+    vis.selectAll(".node")
+        .filter(function(node) {
+                  return (sequenceArray.indexOf(node) >= 0);
+                })
+        .style("opacity", 1);
+  }
+
+  // Restore everything to full opacity when moving off the visualization.
+  function mouseleave(d) {
+
+    // Hide the breadcrumb trail
+    d3.select("#trail")
+        .style("visibility", "hidden");
+
+    // Deactivate all segments during transition.
+    d3.selectAll(".node").on("mouseover", null);
+
+    // Transition each segment to full opacity and then reactivate it.
+    d3.selectAll(".node")
+        .transition()
+        .duration(1000)
+        .style("opacity", 1)
+        .each("end", function() {
+                d3.select(this).on("mouseover", mouseover);
+              });
+  }
+
+  // Given a node in a partition layout, return an array of all of its ancestor
+  // nodes, highest first, but excluding the root.
+  function getAncestors(node) {
+    var path = [];
+    var current = node;
+    while (current.parent) {
+      path.unshift(current);
+      current = current.parent;
+    }
+    return path;
+  }
+
+  function initializeBreadcrumbTrail() {
+    // Add the svg area.
+    var trail = d3.select("#sequence").append("svg:svg")
+        .attr("width", width)
+        .attr("height", 50)
+        .attr("id", "trail");
+    // Add the label at the end, for the percentage.
+    trail.append("svg:text")
+      .attr("id", "endlabel")
+      .style("fill", "#000");
+  }
+
+  // Generate a string that describes the points of a breadcrumb polygon.
+  function breadcrumbPoints(d, i) {
+    var points = [];
+    points.push("0,0");
+    points.push(b.w + ",0");
+    points.push(b.w + b.t + "," + (b.h / 2));
+    points.push(b.w + "," + b.h);
+    points.push("0," + b.h);
+    if (i > 0) { // Leftmost breadcrumb; don't include 6th vertex.
+      points.push(b.t + "," + (b.h / 2));
+    }
+    return points.join(" ");
+  }
+
+  // Update the breadcrumb trail to show the current sequence and percentage.
+  function updateBreadcrumbs(nodeArray, percentageString) {
+
+    // Data join; key function combines name and depth (= position in sequence).
+    var g = d3.select("#trail")
+        .selectAll("g")
+        .data(nodeArray, function(d) { return d.name + d.depth; });
+
+    // Add breadcrumb and label for entering nodes.
+    var entering = g.enter().append("svg:g");
+
+    entering.append("svg:polygon")
+        .attr("points", breadcrumbPoints)
+        .style("fill", function(d) { return colorOf(d.name, d.children); });
+
+    entering.append("svg:text")
+        .attr("x", (b.w + b.t) / 2)
+        .attr("y", b.h / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "middle")
+        .text(function(d) { return d.name; });
+
+    // Set position for entering and updating nodes.
+    g.attr("transform", function(d, i) {
+      return "translate(" + i * (b.w + b.s) + ", 0)";
+    });
+
+    // Remove exiting nodes.
+    g.exit().remove();
+
+    // Now move and update the percentage at the end.
+    d3.select("#trail").select("#endlabel")
+        .attr("x", (nodeArray.length + 0.5) * (b.w + b.s))
+        .attr("y", b.h / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "middle")
+        .text(percentageString);
+
+    // Make the breadcrumb trail visible, if it's hidden.
+    d3.select("#trail")
+        .style("visibility", "");
+
+  }
+
+  function drawLegend() {
+
+    // Dimensions of legend item: width, height, spacing, radius of rounded rect.
+    var li = {
+      w: 75, h: 30, s: 3, r: 3
+    };
+
+    var legend = d3.select("#legend").append("svg:svg")
+        .attr("width", li.w)
+        .attr("height", d3.keys(colors).length * (li.h + li.s));
+
+    var g = legend.selectAll("g")
+        .data(d3.entries(colors))
+        .enter().append("svg:g")
+        .attr("transform", function(d, i) {
+                return "translate(0," + i * (li.h + li.s) + ")";
+             });
+
+    g.append("svg:rect")
+        .attr("rx", li.r)
+        .attr("ry", li.r)
+        .attr("width", li.w)
+        .attr("height", li.h)
+        .style("fill", function(d) { return d.value; });
+
+    g.append("svg:text")
+        .attr("x", li.w / 2)
+        .attr("y", li.h / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "middle")
+        .text(function(d) { return d.key; });
+  }
+
+  function toggleLegend() {
+    var legend = d3.select("#legend");
+    if (legend.style("visibility") == "hidden") {
+      legend.style("visibility", "");
+    } else {
+      legend.style("visibility", "hidden");
+    }
+  }
+
+  // Take a 2-column CSV and transform it into a hierarchical structure suitable
+  // for a partition layout. The first column is a sequence of step names, from
+  // root to leaf, separated by hyphens. The second column is a count of how 
+  // often that sequence occurred.
+  function buildHierarchy(csv) {
+    var root = {"name": "root", "children": []};
+    for (var i = 0; i < csv.length; i++) {
+      var sequence = csv[i][0];
+      var size = +csv[i][1];
+      if (isNaN(size)) { // e.g. if this is a header row
+        continue;
+      }
+      var parts = sequence.split("-");
+      var currentNode = root;
+      for (var j = 0; j < parts.length; j++) {
+        var children = currentNode["children"];
+        var nodeName = parts[j];
+        var childNode;
+        if (j + 1 < parts.length) {
+     // Not yet at the end of the sequence; move down the tree.
+   	var foundChild = false;
+   	for (var k = 0; k < children.length; k++) {
+   	  if (children[k]["name"] == nodeName) {
+   	    childNode = children[k];
+   	    foundChild = true;
+   	    break;
+   	  }
+   	}
+    // If we don't already have a child node for this branch, create it.
+   	if (!foundChild) {
+   	  childNode = {"name": nodeName, "children": []};
+   	  children.push(childNode);
+   	}
+   	currentNode = childNode;
+        } else {
+   	// Reached the end of the sequence; create a leaf node.
+   	childNode = {"name": nodeName, "size": size};
+   	children.push(childNode);
+        }
+      }
+    }
+    return root;
+  };
+
+}
